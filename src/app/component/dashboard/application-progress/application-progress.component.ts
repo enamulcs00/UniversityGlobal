b4cd994b2c5a7af45fd32d3bc07ec1e9322b36e0
Enamul Haque <enamulcs008@gmail.com>
2020-09-05 09:18:12 +0530

    application progress api

--- src/app/component/dashboard/application-progress/application-progress.component.ts
+++ src/app/component/dashboard/application-progress/application-progress.component.ts
@@ -1,5 +1,6 @@
 import { ServicesService } from '../../../services.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-application-progress',
@@ -8,28 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationProgressComponent implements OnInit {
   subscriptionList:  any = [];
-  accountData: any;
-  formId: any;
+  accountDetails: any;
+  form_Id: any;
+  accountData:any;
 
 
-  constructor(private service:ServicesService) { }
+  constructor(private service:ServicesService,private activateRoute:ActivatedRoute) {}
 
   ngOnInit() {
     window.scroll(0,0)
-    this.accountData = JSON.parse(localStorage.getItem('myProfile'))
-    this.accountData.imageUrl = this.accountData.imageUrl ? this.accountData.imageUrl : 'assets/images/pick-1.png';
-    this.getApplicationProgress()
+    this.accountDetails = JSON.parse(localStorage.getItem('myProfile'))
+    this.activateRoute.params.subscribe((res:any) => {
+      this.form_Id = res;
+      console.log('FormResponse'+res)
+      console.log("ResId is-:"+res.id)
+      console.log(this.form_Id)
+      this.getApplicationProgress()
+    }) 
+  }
+ getApplicationProgress(){
+   let formId = this.form_Id.id;
+   let accountData = this.acc
+    this.service.showSpinner()
+    this.service.getApi(`course/get-application-status-for-progress-view?formId=${this.form_Id.id}
+    &representativeId=${this.accountDetails.representativeDetailsId}`,1)
+    .subscribe((res:any) => {
+        console.log('Response is---->',res)
+         this.service.hideSpinner()
+    })
   }
 
-  getApplicationProgress(){
-    this.service.showSpinner()
-    this.service.getApi(`course/get-application-status-for-progress-view`,1).subscribe((res:any) => {
-        console.log('res---->',res)
-        if(res.body.status == 200){
-            this.subscriptionList = res.body.data.resultList.content
-        }
-        this.service.hideSpinner()
-    })
-}
 
 }
+// ngOnInit() {
+//   window.scroll(0,0)
+//   this.accountData = JSON.parse(localStorage.getItem('myProfile'))
+//   this.accountData.imageUrl = this.accountData.imageUrl ? this.accountData.imageUrl : 'assets/images/pick-1.png';
+//   this.getSubscriptionHistoryList()
+// }
+
